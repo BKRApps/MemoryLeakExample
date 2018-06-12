2909a969751f706e7a376f331c49be6d229d1175
kumar reddy <kumar.reddy@swiggy.in>
2018-06-12 21:42:39 +0530

    initial setup

---
+++ MemoryLeakSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryLeakSample
+//
+//  Created by kumar reddy on 12/06/18.
+//  Copyright © 2018 BKRApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
